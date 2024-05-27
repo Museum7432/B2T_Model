586b429ace6664e458901847c8137eb26c6c1d25
21522188 <21522188@gm.uit.edu.vn>
2024-05-28 01:53:16 +0700

    tune sweep

--- sweep.py
+++ sweep.py
@@ -12,10 +12,10 @@ sweep_configuration = {
         "conv_kernel2": {"value": 3},
         "conv_g1": {"value": 256},
         "conv_g2": {"value": 1},
-        "hidden_size": {"value": 512},
-        "encoder_n_layer": {"min": 5, "max": 7},
-        "decoder_n_layer": {"min": 2, "max": 4},
-        "peak_lr": {"values": [3e-4, 2e-4, 1e-4, 5e-5]},
+        "hidden_size": {"value": 768},
+        "encoder_n_layer": {"value": 6},
+        "decoder_n_layer": {"value": 4},
+        "peak_lr": {"values": [3e-4, 2e-4, 1e-4]},
         ##############################
         # this will need another sweep
         # "update_probs": {"max": 0.9, "min": 0.5},
@@ -26,7 +26,7 @@ sweep_configuration = {
         "beta_2": {"value": 0.95},
         "eps": {"value": 1e-08},
         "weight_decay": {"value": 0.1},
-        "lr_warmup_perc": {"values": [0.2, 0.1]},
+        "lr_warmup_perc": {"values": [0.2, 0.1, 0.3]},
         "add_noises": {"value": True},
         "train_data_dir": {"value": "./dataset/train"},
         "val_data_dir": {"value": "./dataset/valid"},
@@ -35,7 +35,7 @@ sweep_configuration = {
         "use_addtional_corpus": {"value": False},
         "sp_noise_std": {"values": [0.2, 0.15, 0.1, 0.05, 0]},
         "features_noise_std": {"values": [0.2, 0.15, 0.1, 0.05, 0]},
-        "gaussian_filter_sigma": {"values": [2, 1, 0.8, 0.5]},
+        "gaussian_filter_sigma": {"values": [1, 0.8, 0.5]},
         "debugging": {"value": False},
         "train_batch_size": {"value": 8},
         # change this
